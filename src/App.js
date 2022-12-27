b56da4385f73c0798adb92c8acf818d87b90c084
vanthinn <thinh209202@gmail.com>
2022-12-27 15:11:08 +0700

    lazy loading

--- src/App.js
+++ src/App.js
@@ -1,38 +1,43 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import Home from "./page/Home";
-import Products from "./page/Products";
 import Header from "./component/Header";
-import Contact from "./page/Contact";
-import Cart from "./page/Cart";
 import Footer from "./component/Footer";
-import ProductDetail from "./page/ProductDetail";
 import Login from "./page/Login";
 import Signup from "./page/Signup";
 
+import React, { lazy, Suspense } from "react";
 import "./App.css";
-import Checkout from "./page/Checkout";
 import NotFound from "./component/NotFound";
+import Loading from "./component/Loading";
+
+const Home = lazy(() => import("./page/Home"));
+const Products = lazy(() => import("./page/Products"));
+const Contact = lazy(() => import("./page/Contact"));
+const Cart = lazy(() => import("./page/Cart"));
+const Checkout = lazy(() => import("./page/Checkout"));
+const ProductDetail = lazy(() => import("./page/ProductDetail"));
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Routes>
-        <Route path="/" element={<Navigate replace to="/home" />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/search/:title" element={<Products />} />
-        <Route
-          path="/products/idproduct/:idproduct"
-          element={<ProductDetail />}
-        />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/home" />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/search/:title" element={<Products />} />
+          <Route
+            path="/products/idproduct/:idproduct"
+            element={<ProductDetail />}
+          />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </div>
   );
